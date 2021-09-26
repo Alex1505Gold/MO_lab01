1bbd692461dea2a696c9c43be113b599f938693f
Alex1505Gold <49805582+Alex1505Gold@users.noreply.github.com>
2021-09-26 18:12:50 +0300

    Add files via upload

--- Simplex.cpp
+++ Simplex.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <fstream>
 
 int main()
 {
@@ -7,24 +8,35 @@ int main()
     int basis[3] = { 1 , 2, 3 };
     int free[3] = { 4, 5, 6 };
     double min = -1;
+    std::ifstream in("input.txt");
     for (int i = 0; i < 3; ++i)
-        std::cin >> s_matrix[3][i + 1];
+        in >> s_matrix[3][i + 1];
+    
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
         {
-            std::cin >> s_matrix[i][j + 1];
+            in >> s_matrix[i][j + 1];
         }
     for (int i = 0; i < 3; ++i)
-        std::cin >> s_matrix[i][0];
+        in >> s_matrix[i][0];
     s_matrix[3][0] = 0;
+    std::cout << "   s0  ";
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << "x" << basis[i] << "  ";
+    }
+    std::cout << "\n";
     for (int i = 0; i < 4; ++i)
     {
+        if (i <=2) std::cout << "x" << free[i] << "  ";
+        else std::cout << "F   ";
         for (int j = 0; j < 4; ++j)
         {
             std::cout << s_matrix[i][j] << "   ";
         }
         std::cout << std::endl;
     }
+    in.close();
     bool f = true;
     int perm_col, perm_str;
     while (f)
@@ -76,11 +88,28 @@ int main()
                 for (int j = 0; j < 4; ++j)
                     s_matrix[i][j] = ad_matrix[i][j];
             std::swap(basis[perm_col - 1], free[perm_str]);
+            std::cout << "   s0  ";
+            for (int i = 0; i < 3; i++)
+            {
+                std::cout << "x" << basis[i] << "  ";
+            }
+            std::cout << "\n";
+            for (int i = 0; i < 4; ++i)
+            {
+                if (i <= 2) std::cout << "x" << free[i] << "  ";
+                else std::cout << "F   ";
+                for (int j = 0; j < 4; ++j)
+                {
+                    std::cout << s_matrix[i][j] << "   ";
+                }
+                std::cout << std::endl;
+            }
         }
         else f = false;
     }
     f = true;
-    while (f)
+    bool unlim = false;
+    while (f && !unlim)
     {
         perm_col = -1;
         min = -1;
@@ -94,6 +123,13 @@ int main()
         }
         if (perm_col > 0)
         {
+            unlim = true;
+            for (int i = 0 ; i < 3; ++i)
+                if (s_matrix[i][perm_col] > 0)
+                {
+                    unlim = false;
+                    break;
+                }
             for (int i = 0; i < 3; ++i)
             {
                 if (((s_matrix[i][0] / s_matrix[i][perm_col]) >= 0) && ((min < 0) || (s_matrix[i][0] / s_matrix[i][perm_col] < min)))
@@ -120,10 +156,30 @@ int main()
                 for (int j = 0; j < 4; ++j)
                     s_matrix[i][j] = ad_matrix[i][j];
             std::swap(basis[perm_col - 1], free[perm_str]);
+            std::cout << "   s0  ";
+            for (int i = 0; i < 3; i++)
+            {
+                std::cout << "x" << basis[i] << "  ";
+            }
+            std::cout << "\n";
+            for (int i = 0; i < 4; ++i)
+            {
+                if (i <= 2) std::cout << "x" << free[i] << "  ";
+                else std::cout << "F   ";
+                for (int j = 0; j < 4; ++j)
+                {
+                    std::cout << s_matrix[i][j] << "   ";
+                }
+                std::cout << std::endl;
+            }
         }
         else f = false;
     }
     std::cout << std::endl;
+    if (unlim)
+    {
+        std::cout << "Function is unlimitted\n";
+    }
     /*
     for (int i = 0; i < 4; ++i)
     {
@@ -137,15 +193,18 @@ int main()
     std::cout << std::endl;
     for (int i = 0; i < 3; ++i) std::cout << free[i] << " ";
     */
-    std::cout << "\nSolution:\n";
-    std::cout << "F = " << s_matrix[3][0] * (-1) << "\n";
-    for (int i = 0; i < 3; ++i)
+    else
     {
-        std::cout << "x" << basis[i] << " = 0\n";
-    }
-    for (int i = 0; i < 3; ++i)
-    {
-        std::cout << "x" << free[i] << " = " << s_matrix[i][0] << "\n";
+        std::cout << "\nSolution:\n";
+        std::cout << "F = " << s_matrix[3][0] * (-1) << "\n";
+        for (int i = 0; i < 3; ++i)
+        {
+            std::cout << "x" << basis[i] << " = 0\n";
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            std::cout << "x" << free[i] << " = " << s_matrix[i][0] << "\n";
+        }
     }
     return 0;
 }
