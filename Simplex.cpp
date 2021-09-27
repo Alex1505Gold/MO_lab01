24f1d2fe1df247e06f2441ab755a03c71d84092d
Alex1505Gold <49805582+Alex1505Gold@users.noreply.github.com>
2021-09-27 14:12:38 +0300

    Add files via upload

--- Simplex.cpp
+++ Simplex.cpp
@@ -39,7 +39,8 @@ int main()
     in.close();
     bool f = true;
     int perm_col, perm_str;
-    while (f)
+    bool exist = true;
+    while (f && exist)
     {
         perm_str = -1;
         perm_col = -1;
@@ -54,60 +55,66 @@ int main()
         }
         if (perm_str >= 0)
         {
+            exist = false;
             for (int i = 1; i < 4; ++i)
             {
                 if (s_matrix[perm_str][i] < 0)
                 {
                     perm_col = i;
+                    exist = true;
                     break;
                 }
             }
-            for (int i = 0; i < 3; ++i)
+            if (exist)
             {
-                if (((s_matrix[i][0] / s_matrix[i][perm_col]) >= 0) && ((min < 0) || (s_matrix[i][0] / s_matrix[i][perm_col] < min)))
+                for (int i = 0; i < 3; ++i)
                 {
-                    min = s_matrix[i][0] / s_matrix[i][perm_col];
-                    perm_str = i;
+                    if (((s_matrix[i][0] / s_matrix[i][perm_col]) >= 0) && ((min < 0) || (s_matrix[i][0] / s_matrix[i][perm_col] < min)))
+                    {
+                        min = s_matrix[i][0] / s_matrix[i][perm_col];
+                        perm_str = i;
+                    }
                 }
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
+                for (int i = 0; i < 4; i++)
                 {
-                    if (i != perm_str && j != perm_col)
-                        ad_matrix[i][j] = s_matrix[i][j] - s_matrix[i][perm_col] * s_matrix[perm_str][j] / s_matrix[perm_str][perm_col];
-                    else if (i != perm_str)
-                        ad_matrix[i][j] = -1 * s_matrix[i][j] / s_matrix[perm_str][perm_col];
-                    else if (j != perm_col)
-                        ad_matrix[i][j] = s_matrix[i][j] / s_matrix[perm_str][perm_col];
-                    else
-                        ad_matrix[i][j] = 1 / s_matrix[i][j];
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (i != perm_str && j != perm_col)
+                            ad_matrix[i][j] = s_matrix[i][j] - s_matrix[i][perm_col] * s_matrix[perm_str][j] / s_matrix[perm_str][perm_col];
+                        else if (i != perm_str)
+                            ad_matrix[i][j] = -1 * s_matrix[i][j] / s_matrix[perm_str][perm_col];
+                        else if (j != perm_col)
+                            ad_matrix[i][j] = s_matrix[i][j] / s_matrix[perm_str][perm_col];
+                        else
+                            ad_matrix[i][j] = 1 / s_matrix[i][j];
+                    }
                 }
-            }
-            for (int i = 0; i < 4; ++i)
-                for (int j = 0; j < 4; ++j)
-                    s_matrix[i][j] = ad_matrix[i][j];
-            std::swap(basis[perm_col - 1], free[perm_str]);
-            std::cout << "   s0  ";
-            for (int i = 0; i < 3; i++)
-            {
-                std::cout << "x" << basis[i] << "  ";
-            }
-            std::cout << "\n";
-            for (int i = 0; i < 4; ++i)
-            {
-                if (i <= 2) std::cout << "x" << free[i] << "  ";
-                else std::cout << "F   ";
-                for (int j = 0; j < 4; ++j)
+                for (int i = 0; i < 4; ++i)
+                    for (int j = 0; j < 4; ++j)
+                        s_matrix[i][j] = ad_matrix[i][j];
+                std::swap(basis[perm_col - 1], free[perm_str]);
+                std::cout << "   s0  ";
+                for (int i = 0; i < 3; i++)
                 {
-                    std::cout << s_matrix[i][j] << "   ";
+                    std::cout << "x" << basis[i] << "  ";
                 }
-                std::cout << std::endl;
+                std::cout << "\n";
+                for (int i = 0; i < 4; ++i)
+                {
+                    if (i <= 2) std::cout << "x" << free[i] << "  ";
+                    else std::cout << "F   ";
+                    for (int j = 0; j < 4; ++j)
+                    {
+                        std::cout << s_matrix[i][j] << "   ";
+                    }
+                    std::cout << std::endl;
+                }
             }
         }
         else f = false;
     }
-    f = true;
+    if (exist) f = true;
+    else std::cout << "There is no solution\n";
     bool unlim = false;
     while (f && !unlim)
     {
